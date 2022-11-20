395642bae9626d9c0bda43950f0a2f637dc5bbe6
Khairul Basar <khairulbashar24853@gmail.com>
2022-11-20 12:03:10 +0600

    Geolocator added...

--- lib/main.dart
+++ lib/main.dart
@@ -5,19 +5,25 @@ import 'package:weather_app_1/widgets/weather_title.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
+import 'package:geolocator/geolocator.dart';
 
 Future<WeatherInfo> fetchWeather() async {
   //const zipCode = "6205";
-  var lat = "24.37019177228249";
-  var lon = "88.6371962195062";
+  var lat = _MyApp().lat;
+  //var lat = "24.37019177228249";
+  //var lon = "88.6371962195062";
+  var lon = _MyApp().lon;
+  print('${lon} in 16');
+  print('${lat} in 17');
+
   final apiKey = "5385ee950371ca21a34d833d96b3a9b7";
   final requestUrl =
-      "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang={ja}";
+      "https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric";
   final response = await http.get(Uri.parse(requestUrl));
   if (response.statusCode == 200) {
     return WeatherInfo.fromJson(jsonDecode(response.body));
   } else {
-    throw Exception("Error loading request URL info");
+    throw Exception("Error loading request URL info ${response.statusCode}");
   }
 }
 
@@ -70,11 +76,66 @@ class MyApp extends StatefulWidget {
 
 class _MyApp extends State<MyApp> {
   late Future<WeatherInfo> futureWeather;
+  bool servicestatus = false;
+  bool haspermission = false;
+  late LocationPermission permission;
+  late Position position;
+  String lat = "24.37019177228249", lon = "88.6371962195062";
+  late StreamSubscription<Position> positionStream;
 
   @override
   void initState() {
+    checkGps();
     super.initState();
+    //checkGps();
     futureWeather = fetchWeather();
+  }
+
+  checkGps() async {
+    servicestatus = await Geolocator.isLocationServiceEnabled();
+    if (servicestatus) {
+      permission = await Geolocator.checkPermission();
+
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          print('Location permissions are denied');
+        } else if (permission == LocationPermission.deniedForever) {
+          print("'Location permissions are permanently denied");
+        } else {
+          haspermission = true;
+        }
+      } else {
+        haspermission = true;
+      }
+
+      if (haspermission) {
+        setState(() {
+          //refresh the UI
+        });
+
+        getLocation();
+      }
+    } else {
+      print("GPS Service is not enabled, turn on GPS location");
+    }
+
+    setState(() {
+      //refresh the UI
+    });
+  }
+
+  getLocation() async {
+    position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position.longitude); //Output: 80.24599079
+    print(position.latitude); //Output: 29.6593457
+
+    lon = position.longitude.toString();
+    lat = position.latitude.toString();
+    setState(() {
+      //refresh UI
+    });
   }
 
   @override
